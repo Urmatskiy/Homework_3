2373379c145d16a482d4f42d4310ef0f2ee3c251
Urmatskiy <nickiton14@gmail.com>
2022-12-07 10:04:27 +0300

    Выполнил 19 задание

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.Write("Напишите пятизначное число: ");
+string stroka = Console.ReadLine();
+if (stroka[0] == stroka[4] && stroka[1] == stroka[3]) Console.Write("Это палиндромон");
+else Console.Write("Это не палиндромон");
