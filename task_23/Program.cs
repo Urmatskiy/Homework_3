fb1ab294daceff744e7e541c5d3cc444669e11af
Urmatskiy <nickiton14@gmail.com>
2022-12-07 10:22:09 +0300

    Выполнил 23 задание

---
+++ task_23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int start = 1;
+while (start <= N)
+{
+    Console.Write((Math.Pow((start), 3)) + ", ");
+    start++;
+}
